8fb3c76f545ee8852ad9d83b51b150401ceeb34a
aquiles <aquilescancinos@gmail.com>
2026-08-12 08:13:49 -0300

    Etapa 7: Layout base

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,5 +1,17 @@
 import { Outlet } from 'react-router'
+import { Sidebar } from './components/Sidebar'
+import { Header } from './components/Header'
 
 export function MainLayout() {
-  return <Outlet />
+  return (
+    <div className="bg-surface-muted flex h-screen">
+      <Sidebar />
+      <div className="flex flex-1 flex-col overflow-hidden">
+        <Header />
+        <main className="flex-1 overflow-y-auto p-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  )
 }
